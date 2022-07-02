49bc5577808967d52b34c234d259b47d4dc23fb4
DESKTOP-LI9PL2G\danil <danilyanov56@gmail.com>
2022-07-03 00:53:02 +0300

    redesigned registration page

--- ui/src/Auth/Register.tsx
+++ ui/src/Auth/Register.tsx
@@ -1,19 +1,133 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import SimpleButton from '../components/Buttons/SimpleButton';
+import Header from '../components/Header/Header';
+import "./Register.css"
 
 interface IRegistrationProps {
 }
 
-class RegistrationPage extends React.Component<IRegistrationProps, any> {
+interface IRegisterPageState {
+    email?: string;
+    password?: string;
+    secondPassword?: string;
+    name?: string;
+}
+
+export default class RegistrationPage extends React.Component<IRegistrationProps, IRegisterPageState> {
+    state: IRegisterPageState = {
+        email: '',
+        password: '',
+        secondPassword: '',
+        name: '',
+    }
+
+    renderEmailInput() {
+        return (
+            <input
+                className='registerInput'
+                onChange={this.onEmailChange}
+                placeholder="Email"
+                value={this.state.email}
+            />
+        )
+    }
+
+    onEmailChange = (e: { target: { value: any; }; }) => {
+        this.setState(state => ({ email: e.target.value }));
+    };
+
+    renderPasswordInput() {
+        return (
+            <input
+                className='registerInput'
+                onChange={this.onPasswordChange}
+                placeholder="Password"
+                value={this.state.password}
+            />
+        )
+    }
+
+    onPasswordChange = (e: { target: { value: any; }; }) => {
+        this.setState(state => ({ password: e.target.value }));
+    };
+
+    renderSecondPasswordInput() {
+        return (
+            <input
+                className='registerInput'
+                onChange={this.onSecondPasswordChange}
+                placeholder="Repeat Password"
+                value={this.state.secondPassword}
+            />
+        )
+    }
+
+    onSecondPasswordChange = (e: { target: { value: any; }; }) => {
+        this.setState(state => ({ secondPassword: e.target.value }));
+    };
+
+    renderNameInput() {
+        return (
+            <input
+                className='registerInput'
+                onChange={this.onNameChange}
+                placeholder="Name"
+                value={this.state.name}
+            />
+        )
+    }
+
+    onNameChange = (e: { target: { value: any; }; }) => {
+        this.setState(state => ({ name: e.target.value }));
+    };
+
+    submit = () => {
+        console.log(this.state?.email)
+        console.log(this.state?.password)
+        console.log(this.state?.secondPassword)
+        console.log(this.state?.name)
+    }
+
     render() {
         return (
-            <div>
-                <span>registration page</span>
-                <p>
-                    <Link to="/">Login</Link>
-                </p>
-            </div>
+            <>
+                <Header
+                    pageTitle='Registration'
+                    rightPartText='Authorize'
+                    rightPartUrl='/'
+                />
+                <div className="registerMainForm">
+                    <div className='registerMainInner'>
+                        <div className='registerFields'>
+                            <div className='registerFieldsGroup'>
+                                <span className='emailText'>Email</span>
+                                {this.renderEmailInput()}
+                            </div>
+                            <div className='registerFieldsGroup'>
+                                <span className='passwordText'>Password</span>
+                                {this.renderPasswordInput()}
+                            </div>
+                            <div className='registerFieldsGroup'>
+                                <span className='passwordText'>Password</span>
+                                {this.renderSecondPasswordInput()}
+                            </div>
+                            <div className='registerFieldsGroup'>
+                                <span className='passwordText'>Password</span>
+                                {this.renderNameInput()}
+                            </div>
+                            <div className='rButton'>
+                                <SimpleButton
+                                    type="submit"
+                                    className="registerButton"
+                                    disabled={false}
+                                    onClick={this.submit}
+                                    text={"Register"}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
-export default RegistrationPage;
