5faa86bb644d3988e1aa6f8c27c55d7f6d31e1e5
Bogdan Smeyko <bogdan.smeyko@gmail.com>
2022-07-02 21:32:57 +0300

    add menu items with icons

--- ui/src/components/LeftMenu/LeftMenu.tsx
+++ ui/src/components/LeftMenu/LeftMenu.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './LeftMenu.css'
+import {CalendarMonth, Groups, ListAlt, Logout} from "@mui/icons-material";
 
 export default class LeftMenu extends React.Component {
     render() {
         return (
             <div className="leftMenu">
                 <div>
+                    <div className="menuItem">
+                        <div>
+                            <Groups fontSize={"large"}/>
+                        </div>
+                        My family
+                    </div>
+                    <div className="menuItem">
+                        <div>
+                            <CalendarMonth fontSize={"large"}/>
+                        </div>
+                        Calendar
+                    </div>
+                    <div className="menuItem">
+                        <div>
+                            <ListAlt fontSize={"large"}/>
+                        </div>
+                        Lists
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div>
+                        <Logout fontSize={"large"}/>
+                    </div>
+                    Logout
                 </div>
             </div>
         );
