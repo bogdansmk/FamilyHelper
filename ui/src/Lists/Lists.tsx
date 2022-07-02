dbe3fb1e11bfbc260401ca12140d37e39be9345f
Bogdan Smeyko <bogdan.smeyko@gmail.com>
2022-07-03 00:05:16 +0300

    add lists page

--- ui/src/Lists/Lists.tsx
+++ ui/src/Lists/Lists.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import Header from "../components/Header/Header";
 import LeftMenu from "../components/LeftMenu/LeftMenu";
+import {Checkbox, FormControlLabel, FormGroup} from "@mui/material";
+import {AddCircle, Star, StarBorder} from "@mui/icons-material";
+import './Lists.css'
 
 export default class ListsPage extends React.Component {
     render() {
@@ -11,6 +13,97 @@ export default class ListsPage extends React.Component {
                         rightPartText=''
                         rightPartUrl=''/>
                 <LeftMenu/>
+                <div className="main">
+                    <div className="mainTitle">Shared Lists</div>
+                    <div className="mainBody">
+                        <div className="card">
+                            <div className="cardTitle">Products
+                                <Checkbox
+                                    icon={<StarBorder color="action"/>}
+                                    checkedIcon={<Star/>}
+                                    color={"warning"}
+                                    size={"medium"}
+                                />
+                            </div>
+                            <div className="cardBody">
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox defaultChecked/>} label="Milk"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Coca-cola"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Sweets"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Apples"/><FormControlLabel control={<Checkbox defaultChecked/>} label="Milk"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Coca-cola"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Sweets"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Apples"/><FormControlLabel control={<Checkbox defaultChecked/>} label="Milk"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Coca-cola"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Sweets"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Apples"/>
+                                </FormGroup>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="cardTitle">Products
+                                <Checkbox
+                                    icon={<StarBorder color="action"/>}
+                                    checkedIcon={<Star/>}
+                                    color={"warning"}
+                                    size={"medium"}
+                                />
+                            </div>
+                            <div className="cardBody">
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox defaultChecked/>} label="Milk"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Coca-cola"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Sweets"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Apples"/>
+                                </FormGroup>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="cardTitle">Products
+                                <Checkbox
+                                    icon={<StarBorder color="action"/>}
+                                    checkedIcon={<Star/>}
+                                    color={"warning"}
+                                    size={"medium"}
+                                />
+                            </div>
+                            <div className="cardBody">
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox defaultChecked/>} label="Milk"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Coca-cola"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Sweets"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Apples"/>
+                                </FormGroup>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="cardTitle">Products
+                                <Checkbox
+                                    icon={<StarBorder color="action"/>}
+                                    checkedIcon={<Star/>}
+                                    color={"warning"}
+                                    size={"medium"}
+                                />
+                            </div>
+                            <div className="cardBody">
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox defaultChecked/>} label="Milk"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Coca-cola"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Sweets"/>
+                                    <FormControlLabel control={<Checkbox/>} label="Apples"/>
+                                </FormGroup>
+                            </div>
+                        </div>
+                        <div className="card addNewCard">
+                            <div className="cardBody">
+                                Add new list
+                                <div className="addNewIcon">
+                                    <AddCircle sx={{ fontSize: 96 }}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
