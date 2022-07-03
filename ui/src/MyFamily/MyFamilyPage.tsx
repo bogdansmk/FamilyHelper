a00e468cdf07dc613c4825abd408e3e8e4890bbe
Bogdan Smeyko <bogdan.smeyko@gmail.com>
2022-07-03 18:10:52 +0300

    add mock MyFamilyPage

---
+++ ui/src/MyFamily/MyFamilyPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from "../components/Header/Header";
+import LeftMenu from "../components/LeftMenu/LeftMenu";
+import {MenuItems} from "../utils/constants";
+import './MyFamilyPage.css'
+import {Tab, Tabs} from "@mui/material";
+
+export default class MyFamilyPage extends React.Component {
+    render() {
+        return (
+            <>
+                <Header pageTitle={'My family'} rightPartText={''} rightPartUrl={''}/>
+                <LeftMenu items={MenuItems}/>
+                <div className="main myFamilyMain">
+                    <Tabs value={'posts'}>
+                        <Tab value="posts" label="Posts" />
+                        <Tab value="map" label="Map" />
+                    </Tabs>
+                </div>
+                <div className="ourFamily">
+                    <div className="ourFamilyTitle">Our family</div>
+                    <div className="ourFamilyBody">
+
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
